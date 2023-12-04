cabf74ed0f04e5b4a92e0382215b6b358ef18e4a
SeraphinaShi <junmingshi67@gmail.com>
2023-12-03 21:45:49 -0800

    fix sl3 error

--- scripts/05_VIA.R
+++ scripts/05_VIA.R
@@ -110,135 +110,16 @@ get_df_sl <- function(sets, smaller_df = T, prev_block = F, inputation = "median
             var_name_list$outcome,
             vars)
   
-  df_sl3 <- dat %>% dplyr::select(vars)
+  df_sl3 <- dat %>% dplyr::select(all_of(vars))
   
   if(inputation == "CCA"){
     df_sl3 <- df_sl3[complete.cases(df_sl3), ] 
   } else if(inputation == "random_unif"){
-    df_sl3 <- dat_r_imp %>% dplyr::select(vars)
+    df_sl3 <- dat_r_imp %>% dplyr::select(all_of(vars))
   } else {
-    df_sl3 <- dat_imp %>% dplyr::select(vars)
+    df_sl3 <- dat_imp %>% dplyr::select(all_of(vars))
   }
   return(df_sl3)
-}
-
-run_sl <- function(df_in, rf_only = T, check_VIA = F){
-    
-  df_here <- df_in %>% select(-half_day)
-  
-  ## define sl3 task
-  
-  # define the folds with clusters and stratification
-  set.seed(123)
-  folds <- make_folds(nrow(df_here), cluster_ids = df_here$ICUSTAY_ID)
-  
-  # define the prediction task
-  sl3_task <- make_sl3_Task(
-    data = df_here, 
-    covariates = colnames(df_here)[-which(names(df_here) %in% c(var_name_list$outcome,
-                                                              var_name_list$ID))],
-    outcome = var_name_list$outcome,
-    outcome_type = "binomial",
-    id = "ICUSTAY_ID",
-    folds = folds
-  )
-  #====================
-  
-  ######################### build library for discrete SL ########################
-  
-  ######## learners without screeners
-  if (rf_only){
-    lrn_mean <- Lrnr_mean$new()
-    lrn_ranger <- Lrnr_ranger$new()
-
-    candidate_lrnrs <- c(lrn_mean, lrn_ranger)
-    names(candidate_lrnrs) <- c("Mean", "Ranger")
-    candidate_lrnrs_stack <- Stack$new(candidate_lrnrs) 
-    
-  } else {
-    # ,  , , , , , , , and the Ensemble SL itself
-
-    lrn_glm <- Lrnr_glm$new() # Generalized Linear Model (GLM)
-    lrn_earth <- Lrnr_earth$new() # Generalized Additive Model (GAM)
-    lrn_ranger <- Lrnr_ranger$new() # Random Forests, 
-    lrn_bayesglm <- Lrnr_pkg_SuperLearner$new("SL.bayesglm") # Bayesian GLM,
-    lrn_lasso <- make_learner(Lrnr_glmnet) # Lasso Regression
-    lrn_ridge <- Lrnr_glmnet$new(alpha = 0) # Ridge Regression
-    lrn_enet.5 <- make_learner(Lrnr_glmnet, alpha = 0.5) # Elastic-net Penalized Regression
-    lrn_polspline <- Lrnr_polspline$new() # Multivariate Adaptive Regression Splines
-    Lrn_lightgbm <- Lrnr_lightgbm$new() # Gradient Boosting Machine
-    Lrn_dbarts <- Lrnr_dbarts$new() # Bayesian Additive Regression Trees
-    
-    base_lrnrs <- c(lrn_glm, lrn_earth, lrn_ranger, lrn_bayesglm, lrn_lasso, lrn_ridge, lrn_enet.5, lrn_polspline, lrn_polspline, Lrn_lightgbm, Lrn_dbarts)
-    names(base_lrnrs) <- c("GLM", "Gam", "RD", "BayesianGLM", "Lassso", "Ridge", "ElasticNet", "Splines", "XGBoost", "BayesianTrees")
-    base_lrnrs_stack <- Stack$new(base_lrnrs) 
-    
-    sl_default <- Lrnr_sl$new(learners = base_lrnrs_stack)
-    
-    candidate_lrnrs <- c(sl_default, base_lrnrs) 
-    names(candidate_lrnrs)[1:3] <- c("SL_defaultMeta", names(base_lrnrs))
-    candidate_lrnrs_stack <- Stack$new(candidate_lrnrs)
-    
-  }
-  
-  ########################### Train discrete SL ##################################
-  
-  discrete <- Lrnr_cv_selector$new(loss_loglik_binomial)
-  
-  sl <- Lrnr_sl$new(learners = candidate_lrnrs_stack, metalearner = discrete)
-  
-  
-  ############# how to run without parallelization: 
-  set.seed(45)
-  fit <- sl$train(sl3_task)
-  
-  ############################# summarize results ################################
-  
-  # lines 71 and 72 should be the same
-  print(fit$coefficients)
-  print(fit$fit_object$cv_meta_fit$fit_object$coef)
-  
-  cv_risk_table_NLL <- fit$cv_risk(eval_fun = loss_loglik_binomial)
-  cv_risk_table_NLL <- cv_risk_table_NLL[cv_risk_table_NLL$learner == "Ranger", -c(1,2)] %>% 
-    rename(se_nll = se, fold_sd_nll = fold_sd) %>% 
-    mutate(ci_l_nll = NLL - 1.96 * se_nll,
-           ci_u_nll = NLL + 1.96 * se_nll,)
-  
-  auc_eval_function <- custom_ROCR_risk("auc")
-  cv_risk_table_AUC <- fit$cv_risk(eval_fun = auc_eval_function)
-  cv_risk_table_AUC <- cv_risk_table_AUC[cv_risk_table_AUC$learner == "Ranger", -c(1,2)] %>% 
-    rename(se_auc = se, fold_sd_auc = fold_sd) %>% 
-    mutate(ci_l_auc = auc - 1.96 * se_auc,
-           ci_u_auc = auc + 1.96 * se_auc,)
-  
-  # area under the precision recall curve is recommended for rare outcomes
-  aucpr_eval_function <- custom_ROCR_risk("aucpr")
-  cv_risk_table_AUCPR <- fit$cv_risk(eval_fun = aucpr_eval_function)
-  cv_risk_table_AUCPR <- cv_risk_table_AUCPR[cv_risk_table_AUCPR$learner == "Ranger", -c(1,2)] %>% 
-    rename(se_aucpr = se, fold_sd_aucpr = fold_sd) %>% 
-    mutate(ci_l_aucpr = aucpr - 1.96 * se_aucpr,
-           ci_u_aucpr = aucpr + 1.96 * se_aucpr,)
-  
-  # 
-  eval_tables <- list("NLL" = cv_risk_table_NLL, "AUC" = cv_risk_table_AUC, "AUCPR" = cv_risk_table_AUCPR)
-  
-  if (check_VIA){
-      importance_measure <- importance(fit = fit, 
-                                     eval_fun = loss_loglik_binomial
-                                     # type = "permute", 
-                                     # covariate_groups = list("assets" = assets)
-    )
-  } else {
-    importance_measure <- NA
-  }
-
-  
-  rtns <- list("sl_fit" = fit, 
-               "importance_measure" = importance_measure,
-               "NLL_summary" = cv_risk_table_NLL, 
-               "AUC_summary" = cv_risk_table_AUC, 
-               "AUCPR_summary" = cv_risk_table_AUCPR)
-  return(rtns)
 }
 
 exp_var_lists <- list(c("W", "VS", "LT"),
@@ -255,7 +136,109 @@ exp_var_lists <- list(c("W", "VS", "LT"),
 ## -----------------------------------------------------------------------------------------------------------
 df_sl3 <- get_df_sl(sets = c("W", "VS", "LT", "n_W", "n_VS", "h_m_VS", "n_LT"), prev_block = F)
 
-rslts <- run_sl(df_sl3, rf_only = F)
+
+
+df_sl3 <- df_sl3 %>% select(-half_day)
+
+set.seed(123)
+df_sl3 <- df_sl3[c(sample(nrow(df_sl3), 100), which(df_sl3$Y_t_plus_1 == 1)), ]
+## define sl3 task
+
+# define the folds with clusters and stratification
+set.seed(123)
+folds <- make_folds(nrow(df_sl3), cluster_ids = df_sl3$ICUSTAY_ID)
+
+# define the prediction task
+sl3_task <- make_sl3_Task(
+  data = df_sl3, 
+  covariates = colnames(df_sl3)[-which(names(df_sl3) %in% c(var_name_list$outcome,
+                                                              var_name_list$ID))],
+  outcome = var_name_list$outcome,
+  outcome_type = "binomial",
+  id = "ICUSTAY_ID",
+  folds = folds
+)
+  #====================
+  
+  ######################### build library for discrete SL ########################
+  
+
+# ,  , , , , , , , and the Ensemble SL itself
+
+lrn_glm <- Lrnr_glm$new() # Generalized Linear Model (GLM)
+lrn_earth <- Lrnr_earth$new() # Generalized Additive Model (GAM)
+lrn_ranger <- Lrnr_ranger$new() # Random Forests, 
+lrn_bayesglm <- Lrnr_pkg_SuperLearner$new("SL.bayesglm") # Bayesian GLM,
+lrn_lasso <- make_learner(Lrnr_glmnet) # Lasso Regression
+lrn_ridge <- Lrnr_glmnet$new(alpha = 0) # Ridge Regression
+lrn_enet.5 <- make_learner(Lrnr_glmnet, alpha = 0.5) # Elastic-net Penalized Regression
+lrn_polspline <- Lrnr_polspline$new() # Multivariate Adaptive Regression Splines
+Lrn_lightgbm <- Lrnr_lightgbm$new() # Gradient Boosting Machine
+Lrn_dbarts <- Lrnr_dbarts$new() # Bayesian Additive Regression Trees
+
+base_lrnrs <- c(lrn_glm, lrn_earth, lrn_ranger, lrn_bayesglm, lrn_lasso, lrn_ridge, lrn_enet.5, Lrn_lightgbm, Lrn_dbarts) # Lrn_lightgbm, Lrn_dbarts
+names(base_lrnrs) <- c("GLM", "Gam", "RF", "BayesianGLM", "Lassso", "Ridge", "ElasticNet", "XGBoost", "BayesianTrees") # "XGBoost", "BayesianTrees"
+
+# base_lrnrs <- c(lrn_glm, lrn_earth, lrn_ranger, lrn_bayesglm, lrn_lasso, lrn_ridge, lrn_enet.5, lrn_polspline, lrn_polspline) # Lrn_lightgbm, Lrn_dbarts
+# names(base_lrnrs) <- c("GLM", "Gam", "RF", "BayesianGLM", "Lassso", "Ridge", "ElasticNet", "Splines") # "XGBoost", "BayesianTrees"
+
+base_lrnrs_stack <- Stack$new(base_lrnrs) 
+
+sl_default <- Lrnr_sl$new(learners = base_lrnrs_stack)
+
+candidate_lrnrs <- c(sl_default, base_lrnrs) 
+names(candidate_lrnrs)[1:3] <- c("SL_defaultMeta", names(base_lrnrs))
+candidate_lrnrs_stack <- Stack$new(candidate_lrnrs)
+    
+
+
+########################### Train discrete SL ##################################
+
+discrete <- Lrnr_cv_selector$new(loss_loglik_binomial)
+
+sl <- Lrnr_sl$new(learners = candidate_lrnrs_stack, metalearner = discrete)
+
+
+############# how to run without parallelization: 
+set.seed(45)
+fit <- sl$train(sl3_task)
+# save.image(file=here("data", "rdata", "05_VIA_sl3_all_0.RData"))
+
+
+
+############################# summarize results ################################
+
+# lines 71 and 72 should be the same
+print(fit$coefficients)
+print(fit$fit_object$cv_meta_fit$fit_object$coef)
+
+print(fit$learner_fits$SL_defaultMeta)
+
+cv_risk_table_NLL <- fit$cv_risk(eval_fun = loss_loglik_binomial)
+cv_risk_table_NLL <- cv_risk_table_NLL %>% 
+  rename(se_nll = se, fold_sd_nll = fold_sd) %>% 
+  mutate(ci_l_nll = NLL - 1.96 * se_nll,
+         ci_u_nll = NLL + 1.96 * se_nll,)
+
+print(cv_risk_table_NLL)
+
+auc_eval_function <- custom_ROCR_risk("auc")
+cv_risk_table_AUC <- fit$cv_risk(eval_fun = auc_eval_function)
+cv_risk_table_AUC <- cv_risk_table_AUC %>% rename(se_auc = se, fold_sd_auc = fold_sd) %>% 
+  mutate(ci_l_auc = auc - 1.96 * se_auc,
+         ci_u_auc = auc + 1.96 * se_auc,)
+
+print(cv_risk_table_AUC)
+
+# area under the precision recall curve is recommended for rare outcomes
+aucpr_eval_function <- custom_ROCR_risk("aucpr")
+cv_risk_table_AUCPR <- fit$cv_risk(eval_fun = aucpr_eval_function)
+cv_risk_table_AUCPR <- cv_risk_table_AUCPR %>% rename(se_aucpr = se, fold_sd_aucpr = fold_sd) %>% 
+  mutate(ci_l_aucpr = aucpr - 1.96 * se_aucpr,
+         ci_u_aucpr = aucpr + 1.96 * se_aucpr,)
+
+print(cv_risk_table_AUCPR)
+
 
 save.image(file=here("data", "rdata", "05_VIA_sl3_all.RData"))
 
